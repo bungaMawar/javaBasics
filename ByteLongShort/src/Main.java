4f72d5284afa8192dc46e5d781c4c6a9e109fef3
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-06 15:47:16 -0400

    Additional int stuff

--- ByteLongShort/src/Main.java
+++ ByteLongShort/src/Main.java
@@ -1,12 +1,14 @@
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
+
+        int value = 10000;
         int minIntVal = Integer.MIN_VALUE;
         int maxIntVal = Integer.MAX_VALUE;
         System.out.println("Minimum value of integer is " + minIntVal);
         System.out.println("Maximum value of integer is " + maxIntVal);
+        System.out.println("Busted int max value: " + (maxIntVal + 1));
+        System.out.println("Busted int min value: " + (minIntVal - 1));
 
-        
+        int maxIntTest = 2_147_483_647;
     }
 }
