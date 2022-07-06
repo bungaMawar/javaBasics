1173262c8b291c7f15f6e75430d53883dbe6306c
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-06 11:50:10 -0400

    Max and min values of integer

--- ByteLongShort/src/Main.java
+++ ByteLongShort/src/Main.java
@@ -1,5 +1,12 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int minIntVal = Integer.MIN_VALUE;
+        int maxIntVal = Integer.MAX_VALUE;
+        System.out.println("Minimum value of integer is " + minIntVal);
+        System.out.println("Maximum value of integer is " + maxIntVal);
+
+        
     }
 }
