3119fd8fc1c17707716159a6892a8d240f51f884
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-06 15:54:47 -0400

    Bytes, Shorts and Longs added

--- ByteLongShort/src/Main.java
+++ ByteLongShort/src/Main.java
@@ -10,5 +10,23 @@ public class Main {
         System.out.println("Busted int min value: " + (minIntVal - 1));
 
         int maxIntTest = 2_147_483_647;
+
+        byte minByteVal = Byte.MIN_VALUE;
+        byte maxByteVal = Byte.MAX_VALUE;
+        System.out.println("Minimum value of byte is " + minByteVal);
+        System.out.println("Maximum value of byte is " + maxByteVal);
+
+        short minShortVal = Short.MIN_VALUE;
+        short maxShortVal = Short.MAX_VALUE;
+        System.out.println("Minimum value of short is " + minShortVal);
+        System.out.println("Maximum value of short is " + maxShortVal);
+
+        long longVal = 100L;
+        long minLongVal = Long.MIN_VALUE;
+        long maxLongVal = Long.MAX_VALUE;
+        System.out.println("Minimum value of long is " + minLongVal);
+        System.out.println("Maximum value of long is " + maxLongVal);
+        long longLiteralVal = 2_147_483_649L;
+        System.out.println(longLiteralVal);
     }
 }
