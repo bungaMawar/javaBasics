8b3b8b3274c2935277f01aafc04961cd8d1c04d3
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-07 18:59:05 -0400

    Char and boolean

---
+++ CharAndBoolean/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        char charVal = 'S';
+        char unicodeVal = '\u0053';
+
+        System.out.println(charVal);
+        System.out.println(unicodeVal);
+
+        char copyrightVal = '\u00A9';
+        System.out.println(copyrightVal);
+
+        boolean trueVal = true;
+        boolean falseVal = false;
+
+        boolean isPersonOverTwentyOne = false;
+    }
+}
