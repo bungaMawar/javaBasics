3a46df90f104fbcb3cf5471cd60d5439d28db677
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-07 17:01:55 -0400

    Floating point challenge, lbs to kg converter

---
+++ FloatingPointChallenge/src/Main.java
@@ -0,0 +1,10 @@
+import java.io.FilterOutputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        double pounds = 150;
+        double resultInKg = pounds * 0.45359237;
+
+        System.out.println(pounds + " lbs to kg: " + resultInKg);
+    }
+}
