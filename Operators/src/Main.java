437e672b2153c725c4ec6948e003a7b3195c8852
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-09 11:26:02 -0400

    Operators

---
+++ Operators/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args){
+        int result = 1 + 2; //1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+        int prevResult = result;
+        result = result - 1;
+        System.out.println("3 - 1 = " + result);
+        System.out.println("prevResult = " + prevResult);
+
+        result = result * 10; //2 * 10 = 20
+        System.out.println("2 * 10 = " + result);
+
+        result = result/5; //20/5 = 4
+        System.out.println("20/5 = " + result);
+
+        result = result % 3; // 4 % 3 = 1
+        System.out.println("4 % 3 = " + result);
+    }
+}
