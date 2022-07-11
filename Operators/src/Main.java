e0ef68d54de97849982b7444f45d083182e0d941
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-11 11:08:17 -0400

    if then statement

--- Operators/src/Main.java
+++ Operators/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int result = 1 + 2; //1 + 2 = 3
         System.out.println("1 + 2 = " + result);
         int prevResult = result;
@@ -10,7 +10,7 @@ public class Main {
         result = result * 10; //2 * 10 = 20
         System.out.println("2 * 10 = " + result);
 
-        result = result/5; //20/5 = 4
+        result = result / 5; //20/5 = 4
         System.out.println("20/5 = " + result);
 
         result = result % 3; // 4 % 3 = 1
@@ -35,5 +35,11 @@ public class Main {
 
         result -= 2;
         System.out.println("10 - 2 = " + result);
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It's not an alien.");
+
+        }
     }
 }
