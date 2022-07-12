1421f2ba6eedcc33c54985a41a8fc59129c6d53f
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-12 18:32:33 -0400

    Parameters

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -5,26 +5,14 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        calculateScore();
+        calculateScore(gameOver, score, levelCompleted, bonus);
 
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-
-        if (gameOver){
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
+        calculateScore(true, 10000, 8, 200);
     }
-    public static void calculateScore(){
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
-
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
         if(gameOver){
             int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
+            finalScore += 2000;
             System.out.println("Your final score was " + finalScore);
         }
     }
