6ded64ff24a122d0520b5229c4345a9dbdaa97da
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-12 15:12:12 -0400

    Statements, whitespace and indentation

---
+++ Statements/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int variable = 50; //this whole line is a statement
+        variable++;
+        variable--;
+        System.out.println("This whole line is a statement too.");
+
+        System.out.println("This is " +
+                "also a " +
+                "statement.");
+
+        int anotherVariable = 50; variable++; //this is two statements
+
+        
+    }
+}
