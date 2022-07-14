d1452589a4cf74a2e4f255743a6bc42bc4962952
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-14 12:48:16 -0400

    Decimal Comparator

---
+++ CodingExercises/DecimalComparator/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        double firstNumber = 3.1756;
+        double secondNumber = 3.175;
+        System.out.println(firstNumber + " and " + secondNumber +
+                " are equal up to three decimal places: " +
+                areEqualByThreeDecimalPlaces(firstNumber, secondNumber));
+    }
+
+    public static boolean areEqualByThreeDecimalPlaces(double firstNumber, double secondNumber){
+        double scale = Math.pow(firstNumber, 3);
+        firstNumber = Math.round(firstNumber * scale)/scale;
+        secondNumber = Math.round(secondNumber * scale)/scale;
+
+        if(firstNumber == secondNumber){
+            return true;
+        }
+        return false;
+    }
+}
