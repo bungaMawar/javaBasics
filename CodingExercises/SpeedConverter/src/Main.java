9cbc742aff2e5482a30f9ba6618c14541153523f
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-14 11:52:41 -0400

    SpeedConverter exercise

---
+++ CodingExercises/SpeedConverter/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour){
+        long milesPerHour = -1;
+        if(kilometersPerHour >= 0){
+            milesPerHour = Math.round(kilometersPerHour/1.609);
+        }
+        return milesPerHour;
+    }
+}
