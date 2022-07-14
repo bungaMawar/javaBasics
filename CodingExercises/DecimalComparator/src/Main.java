b0549cb8acd70c1969f642b2a6d10f88419d8fe8
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-14 12:54:04 -0400

    Checking for when numbers are 0 because they don't round right apparently

--- CodingExercises/DecimalComparator/src/Main.java
+++ CodingExercises/DecimalComparator/src/Main.java
@@ -5,9 +5,18 @@ public class Main {
         System.out.println(firstNumber + " and " + secondNumber +
                 " are equal up to three decimal places: " +
                 areEqualByThreeDecimalPlaces(firstNumber, secondNumber));
+
+        firstNumber = 0d;
+        secondNumber = 0d;
+        System.out.println(firstNumber + " and " + secondNumber +
+                " are equal up to three decimal places: " +
+                areEqualByThreeDecimalPlaces(firstNumber, secondNumber));
     }
 
     public static boolean areEqualByThreeDecimalPlaces(double firstNumber, double secondNumber){
+        if(firstNumber == 0d && secondNumber == 0d){
+            return true;
+        }
         double scale = Math.pow(firstNumber, 3);
         firstNumber = Math.round(firstNumber * scale)/scale;
         secondNumber = Math.round(secondNumber * scale)/scale;
