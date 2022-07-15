4da7a77e4bcfcc18010175408370d0c94f182540
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-15 17:04:09 -0400

    made conditions inclusive

--- CodingExercises/PlayingCat/src/Main.java
+++ CodingExercises/PlayingCat/src/Main.java
@@ -7,12 +7,12 @@ public class Main {
 
     public static boolean isCatPlaying(boolean summer, int temperature){
         if(summer){
-            if(temperature <= 45 && temperature > 25){
+            if(temperature <= 45 && temperature >= 25){
                 return true;
             }
         }
         else{
-            if(temperature <= 35 && temperature > 25) {
+            if(temperature <= 35 && temperature >= 25) {
                 return true;
             }
         }
