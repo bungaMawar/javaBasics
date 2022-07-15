434265667ceb457db64ed539816ad1daf7f85f1e
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-15 16:36:10 -0400

    v had to be uppercase for autocheck

--- CodingExercises/MinutesToYearsAndDaysCalculator/src/com/shafiya/java/Main.java
+++ CodingExercises/MinutesToYearsAndDaysCalculator/src/com/shafiya/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void printYearsAndDays(long minutes){
         if(minutes < 0){
-            System.out.println("Invalid value");
+            System.out.println("Invalid Value");
         }
         else{
             long hours = minutes/60;
