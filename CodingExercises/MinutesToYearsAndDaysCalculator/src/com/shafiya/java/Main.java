c5c0f9a57aab97e39d2fa8c65bb9158ce7a91082
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-15 16:34:53 -0400

    added else to if statement

--- CodingExercises/MinutesToYearsAndDaysCalculator/src/com/shafiya/java/Main.java
+++ CodingExercises/MinutesToYearsAndDaysCalculator/src/com/shafiya/java/Main.java
@@ -8,11 +8,13 @@ public class Main {
         if(minutes < 0){
             System.out.println("Invalid value");
         }
-        long hours = minutes/60;
-        long days = hours/24;
-        long years = days/365;
-        days = days % 365;
+        else{
+            long hours = minutes/60;
+            long days = hours/24;
+            long years = days/365;
+            days = days % 365;
 
-        System.out.println(minutes + " min = " + years + " y and " + days + " d");
+            System.out.println(minutes + " min = " + years + " y and " + days + " d");
+        }
     }
 }
