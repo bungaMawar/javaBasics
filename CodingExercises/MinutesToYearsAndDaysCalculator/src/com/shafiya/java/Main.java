9da46bba9370985175ae33613e1f5f93df98d84d
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-15 16:32:53 -0400

    added .gitignore and another challenge

---
+++ CodingExercises/MinutesToYearsAndDaysCalculator/src/com/shafiya/java/Main.java
@@ -0,0 +1,18 @@
+package com.shafiya.java;
+public class Main {
+    public static void main(String[] args) {
+        printYearsAndDays(1051200);
+    }
+
+    public static void printYearsAndDays(long minutes){
+        if(minutes < 0){
+            System.out.println("Invalid value");
+        }
+        long hours = minutes/60;
+        long days = hours/24;
+        long years = days/365;
+        days = days % 365;
+
+        System.out.println(minutes + " min = " + years + " y and " + days + " d");
+    }
+}
