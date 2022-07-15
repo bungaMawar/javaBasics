aca381a5aa37ccf280d15ab6950236e2b232e11f
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-15 17:03:05 -0400

    playing cat

---
+++ CodingExercises/PlayingCat/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Cat will play: " + isCatPlaying(true, 10));
+        System.out.println("Cat will play: " + isCatPlaying(false, 36));
+        System.out.println("Cat will play: " + isCatPlaying(false, 35));
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature){
+        if(summer){
+            if(temperature <= 45 && temperature > 25){
+                return true;
+            }
+        }
+        else{
+            if(temperature <= 35 && temperature > 25) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
