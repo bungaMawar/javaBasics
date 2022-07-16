c62fa9fae83b3a3c4ac4b159f029da042b4d7c27
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-16 15:04:24 -0400

    For statement challenge

--- ForStatement/src/com/shafiya/java/Main.java
+++ ForStatement/src/com/shafiya/java/Main.java
@@ -1,13 +1,8 @@
 package com.shafiya.java;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("10,000 at 2% interest: " +  calculateInterest(10000, 2));
-        System.out.println("10,000 at 3% interest: " +  calculateInterest(10000, 3));
-        System.out.println("10,000 at 4% interest: " +  calculateInterest(10000, 4));
-        System.out.println("10,000 at 5% interest: " +  calculateInterest(10000, 5));
-
-        for(int i = 0; i < 5; i++){
-            System.out.println("Loop: " + i);
+        for(int i = 2; i <= 8; i++){
+            System.out.println("10,000 at " + i + "% interest: " +  calculateInterest(10000, i));
         }
     }
 
