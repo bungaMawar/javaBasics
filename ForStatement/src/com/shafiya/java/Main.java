56443342b12745540609d7c2fa6ec20434382bc0
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-16 16:21:23 -0400

    For statement challenge, reverse this time

--- ForStatement/src/com/shafiya/java/Main.java
+++ ForStatement/src/com/shafiya/java/Main.java
@@ -4,6 +4,11 @@ public class Main {
         for(int i = 2; i <= 8; i++){
             System.out.println("10,000 at " + i + "% interest: " +  String.format("%.2f", calculateInterest(10000, i)));
         }
+
+        System.out.println("---------");
+        for(int i = 8; i >= 2; i--){
+            System.out.println("10,000 at " + i + "% interest: " +  String.format("%.2f", calculateInterest(10000, i)));
+        }
     }
 
     public static double calculateInterest(double amount, double interestRate){
