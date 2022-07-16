07c1e79079b324dea3350e997646a11cd0ae7599
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-16 16:19:59 -0400

    formatting decimal places

--- ForStatement/src/com/shafiya/java/Main.java
+++ ForStatement/src/com/shafiya/java/Main.java
@@ -2,7 +2,7 @@ package com.shafiya.java;
 public class Main {
     public static void main(String[] args) {
         for(int i = 2; i <= 8; i++){
-            System.out.println("10,000 at " + i + "% interest: " +  calculateInterest(10000, i));
+            System.out.println("10,000 at " + i + "% interest: " +  String.format("%.2f", calculateInterest(10000, i)));
         }
     }
 
