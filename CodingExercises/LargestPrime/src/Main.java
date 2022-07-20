1670d0655f66fe2527f88d8732fd7ab1223b1ec8
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-20 18:08:19 -0400

    added isPrime method to simplify things

--- CodingExercises/LargestPrime/src/Main.java
+++ CodingExercises/LargestPrime/src/Main.java
@@ -5,21 +5,32 @@ public class Main {
 //        the method should return -1 to indicate an invalid value.
 //        The method should calculate the largest prime factor of a given number and return it.
         System.out.println(getLargestPrime(21));
+        System.out.println(getLargestPrime(7));
+        System.out.println(isPrime(7));
+        System.out.println(isPrime(24));
     }
     public static int getLargestPrime(int number){
         if(number < 0){
             return -1;
         }
         int prime = -1;
-        for(int x = 1; x < number; x++){
+        for(int x = 2; x < number; x++){
             if(number % x == 0){
                 for(int i = 1; i < number/x; i++){
-                    if(!((number/x) % i == 0) && number/x > prime && number/x != number){
+                    if(!((number/x) % i == 0) && number/x > prime){
                         prime = number/x;
                     }
                 }
             }
         }
         return prime;
+    }
+    public static boolean isPrime(int number){
+        for(int x = 2; x < number - 1; x++){
+            if(number % x == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
