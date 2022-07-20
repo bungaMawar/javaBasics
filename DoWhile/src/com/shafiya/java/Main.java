2ad515c2d08911f84edf3db1568925a5cc914b86
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-20 09:57:13 -0400

    While and do while loops

---
+++ DoWhile/src/com/shafiya/java/Main.java
@@ -0,0 +1,26 @@
+package com.shafiya.java;
+public class Main {
+    public static void main(String[] args) {
+        int count = 5;
+        while (count != 5){
+            System.out.println("Count = " + count);
+            count++;
+        }
+        //won't execute
+
+        count = 0;
+        while(true){
+            if(count == 5){
+                break;
+            }
+            count++;
+            System.out.println("Count = " + count);
+        }
+
+        count = 0;
+        do{
+            System.out.println("Count = " + count);
+            count++;
+        }while(count != 6);
+    }
+}
