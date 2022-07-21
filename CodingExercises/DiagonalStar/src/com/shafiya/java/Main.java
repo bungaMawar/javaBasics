90bf4dfa462c1b0229b1497eff14df6dbca452b7
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-21 07:45:22 -0400

    diagonal stars

---
+++ CodingExercises/DiagonalStar/src/com/shafiya/java/Main.java
@@ -0,0 +1,32 @@
+package com.shafiya.java;
+public class Main {
+    public static void main(String[] args) {
+//        Write a method named printSquareStar with one parameter of type int named number.
+//        If number is < 5, the method should print "Invalid Value".
+//        The method should print diagonals to generate a rectangular pattern composed of stars (*).
+//        This should be accomplished by using loops (see examples below).
+
+//        The patterns above consist of a number of rows and columns (where number is the number of rows to print).
+//        For each row or column, stars are printed based on four conditions (Read them carefully):
+//
+//* In the first or last row
+//
+//* In the first or last column
+//
+//* When the row number equals the column number
+//
+//* When the column number equals rowCount - currentRow + 1 (where currentRow is current row number)
+
+    }
+
+    public static void printSquareStar(int number){
+        if(number < 5){
+            System.out.println("Invalid Value");
+        }
+        for(int row = 0; row <= 5; row++){
+            for(int col = 0; col <= 5;  col++){
+
+            }
+        }
+    }
+}
