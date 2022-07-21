ad056fae76ca4b300a4908cdf958dd2d0be985e0
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-20 21:27:14 -0400

    additional methods aren't allowed with autocheck so i went with an alternate solution that took me 5 hours to come up with no joke

--- CodingExercises/LargestPrime/src/Main.java
+++ CodingExercises/LargestPrime/src/Main.java
@@ -6,16 +6,44 @@ public class Main {
 //        The method should calculate the largest prime factor of a given number and return it.
         System.out.println(getLargestPrime(21));
         System.out.println(getLargestPrime(7));
+        System.out.println(getLargestPrime(45));
     }
     public static int getLargestPrime(int number){
-        if(number < 0){
+        if(number <= 1){
             return -1;
         }
+
         int prime = -1;
-        for(int x = 1; x < number; x++) {
-            if (isPrime(number/x) && prime < number/x){
-                prime = number/x;
+        boolean isPrime = true;
+        // is it divisible by anythign other than one and itself
+        for(int x = 2; x < number; x++){
+            if(number % x == 0){
+                isPrime = false;
             }
+        }
+        //if yes then are its dividors primes?
+        if (!isPrime){
+            for(int x = 1; x < number; x++){//dividing number by everything from one to number - 1 to find dividors
+                if(number % x == 0){//is it a dividor
+                    boolean isDividorPrime = true;
+                    for(int i = 2; i < number/x; i++){//check if the dividors are primes
+                        if(number/x % i == 0){//45/9 % 3 == 0
+                            isDividorPrime = false;
+                            break;
+                        }
+                    }
+                    if(isDividorPrime){//computer please why does 9 pass
+                        prime = number/x;
+                        break;
+                    }
+                    else{
+                        prime = -1;
+                    }
+                }
+            }
+        }
+        else{
+            return number;
         }
         return prime;
     }
