f3c0f29dd2e05c949f852c2b3c95dfb990f5add9
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-21 14:07:12 -0400

    Read user input challenge; it's a bit broken for some reason, but it works

---
+++ Challenges/ReadingUserInputChallenge/src/com/shafiya/java/Main.java
@@ -0,0 +1,35 @@
+package com.shafiya.java;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        -Read 10 numbers from the console entered by the user and print the sum of those numbers.
+//                -Create a Scanner like we did in the previous video.
+//                -Use the hasNextInt() method from the scanner to check if the user has entered an int value.
+//        -If hasNextInt() returns false, print the message ÅgInvalid NumberÅh. Continue reading until you have
+//        read 10 numbers.
+//                -Use the nextInt() method to get the number and add it to the sum.
+//                -Before the user enters each number, print the message ÅgEnter number #x:Åh where
+//                x represents the count, i.e. 1, 2, 3, 4, etc.
+//                -For example, the first message printed to the user would be ÅgEnter number #1:Åh,
+//                the next ÅgEnter number #2: Åh, and so on.
+        Scanner scanner = new Scanner(System.in);
+        int count = 1;
+        int sum = 0;
+        while(count <= 10){
+            System.out.print("Enter number #" + count + ": ");
+            int input = 0;
+            count++;
+            input = scanner.nextInt();
+            if(scanner.hasNextInt()){
+                sum += input;
+            }
+            else{
+                System.out.println("Invalid Value.");
+            }
+        }
+        scanner.close();
+        System.out.println("Sum of numbers = " + sum);
+    }
+}
