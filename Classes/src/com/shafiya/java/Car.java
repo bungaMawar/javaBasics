ed8d778567ccb1bb070d45b82ca567438020bde3
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-23 10:36:14 -0400

    class methods; Getters and setters

--- Classes/src/com/shafiya/java/Car.java
+++ Classes/src/com/shafiya/java/Car.java
@@ -1,9 +1,25 @@
 package com.shafiya.java;
 
+import java.util.Locale;
+
 public class Car {
     private int doors;
     private int wheels;
     private String model;
     private String engine;
     private String color;
+
+    public void setModel(String model){
+        String validModel = model.toLowerCase();
+        if(validModel.equals("civic") || validModel.equals("pilot")) {
+            this.model = model;
+        }
+        else{
+            this.model = "Unknown";
+        }
+    }
+
+    public String getModel(){
+        return this.model;
+    }
 }
--- Classes/src/com/shafiya/java/Main.java
+++ Classes/src/com/shafiya/java/Main.java
@@ -2,6 +2,11 @@ package com.shafiya.java;
 public class Main {
     public static void main(String[] args) {
         Car honda = new Car();
-        
+
+        System.out.println("Model is " + honda.getModel());
+        honda.setModel("Civic");
+        System.out.println("Model is " + honda.getModel());
+        honda.setModel("Oddessy");
+        System.out.println("Model is " + honda.getModel());
     }
 }
