c32f0398d3082e37a09b164631c7a9dae186eed4
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-07-31 08:19:42 -0400

    Alternate solution + some fixes

--- Challenges/EncapsulationChallenge/src/com/shafiya/java/Main.java
+++ Challenges/EncapsulationChallenge/src/com/shafiya/java/Main.java
@@ -9,16 +9,21 @@ public class Main {
         // Add methods to fill up the toner (up to a maximum of 100%), another method to
         // simulate printing a page (which should increase the number of pages printed).
         // Decide on the scope, whether to use constructors, and anything else you think is needed.
-        Printer printer = new Printer(100, 20, false);
-        Printer secondPrinter = new Printer(10, 50, true);
+        Printer printer = new Printer(100, false);
+        Printer secondPrinter = new Printer(10, true);
 
-        printer.print();
+        printer.print(20);
         System.out.println("Toner is " + printer.getToner() + "% filled.");
         printer.fillToner(100);
+        printer.print(25);
 
-        secondPrinter.print();
+        secondPrinter.print(50);
         secondPrinter.fillToner(50);
-        secondPrinter.print();
+        secondPrinter.print(50);
         System.out.println("Toner is " + secondPrinter.getToner() + "% filled.");
+        secondPrinter.print(25);
+        System.out.println(secondPrinter.getPages() + " pages printed.");
+        System.out.println("Toner is " + secondPrinter.getToner() + "% filled.");
+
     }
 }
--- Challenges/EncapsulationChallenge/src/com/shafiya/java/Printer.java
+++ Challenges/EncapsulationChallenge/src/com/shafiya/java/Printer.java
@@ -2,40 +2,47 @@ package com.shafiya.java;
 
 public class Printer {
     private int toner;
-    private int pages;
+    private int pages = 0;
     private boolean isDuplex;
 
-    public Printer(int toner, int pages, boolean isDuplex) {
+    public Printer(int toner, boolean isDuplex) {
         this.toner = toner;
         if(this.toner < 0){
             this.toner = 0;
         } else if(this.toner > 100){
             this.toner = 100;
         }
-        this.pages = pages;
         this.isDuplex = isDuplex;
     }
 
     public void fillToner(int fillAmount){
+        if(fillAmount < 0){
+            fillAmount = 0;
+        } else if(fillAmount > 100){
+            fillAmount = 100;
+        }
+
         this.toner += fillAmount;
+
         if(this.toner < 0){
             this.toner = 0;
         } else if(this.toner > 100){
             this.toner = 100;
         }
+
         System.out.println("Toner is " + this.toner + "% filled.");
     }
-    public void print(){
+    public void print(int pages){
         if(isDuplex){
-            this.pages = this.pages/2;
+            pages = (pages/2) + (pages % 2);
         }
-
-        System.out.println("Printing " + this.pages + " pages.");
+        this.pages = pages;
+        System.out.println("Printing " + pages + " pages.");
         int counter = 1;
-        while(this.pages > 0 && this.toner > 0){
+        while(pages > 0 && this.toner > 0){
             System.out.println("Printing page #" + counter);
             counter++;
-            this.pages--;
+            pages--;
             this.toner--;
         }
         if(pages > 0){
