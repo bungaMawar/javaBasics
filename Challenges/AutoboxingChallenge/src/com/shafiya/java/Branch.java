f995f9a7ce468024b518b5c4f7b02353ce435323
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-08-11 17:59:33 -0400

    implemented bank, branch, and customer classes

---
+++ Challenges/AutoboxingChallenge/src/com/shafiya/java/Branch.java
@@ -0,0 +1,51 @@
+package com.shafiya.java;
+
+import java.util.ArrayList;
+
+public class Branch {
+    private ArrayList<Customer> customerList;
+    private String name;
+
+    public Branch(String name) {
+        this.name = name;
+        this.customerList = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Customer> getCustomerList() {
+        return customerList;
+    }
+
+    public boolean addCustomer(String customerName, double initialAmount){
+        if(searchCustomer(name) == null){
+            this.customerList.add(new Customer(customerName, initialAmount));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addTransaction(double transactionAmount, String name){
+        Customer existingCustomer = searchCustomer(name);
+        if(existingCustomer != null){
+            existingCustomer.addTransaction(transactionAmount);
+            return true;
+        }
+        return false;
+    }
+
+    public int searchCustomer(Customer customer){
+        return this.customerList.indexOf(customer);
+    }
+    private Customer searchCustomer(String name){
+        for (int x = 0; x < this.customerList.size(); x++){
+            Customer customer = this.customerList.get(x);
+            if(customer.equals(name)){
+                return customer;
+            }
+        }
+        return null;
+    }
+}
