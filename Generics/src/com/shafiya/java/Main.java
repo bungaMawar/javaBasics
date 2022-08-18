d48d8b7e1a79e9271f59e20442ec6986f5a4fb30
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2022-08-18 18:45:20 -0400

    Intro to generics

---
+++ Generics/src/com/shafiya/java/Main.java
@@ -0,0 +1,24 @@
+package com.shafiya.java;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> items = new ArrayList<>();
+        items.add(1);
+        items.add(2);
+        items.add(3);
+//        items.add("fiya");
+        items.add(4);
+        items.add(5);
+        items.add(6);
+
+        printDoubled(items);
+    }
+
+    private static void printDoubled(ArrayList<Integer> n){
+        for(int i : n){
+            System.out.println(i * 2);
+        }
+    }
+}
